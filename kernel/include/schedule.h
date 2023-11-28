2309b9cfc338865ccda6ac8fd41f4c88e84b24d0
Alex Boxall <alexboxall3@gmail.com>
2023-11-28 14:41:23 +1100

    Update schedule.h

--- kernel/include/schedule.h
+++ kernel/include/schedule.h
@@ -2,6 +2,15 @@
 
 #include <common.h>
 
+#define SCHEDULE_POLICY_FIXED             0
+#define SCHEUDLE_POLICY_USER_HIGHER       1
+#define SCHEDULE_POLICY_USER_NORMAL       2
+#define SCHEDULE_POLICY_USER_LOWER        3
+
+#define FIXED_PRIORITY_KERNEL_HIGH        0
+#define FIXED_PRIORITY_KERNEL_NORMAL      30
+#define FIXED_PRIORITY_IDLE               255
+
 void Schedule(void);
 void ScheduleWithLockHeld(void);
 void LockScheduler(void);
