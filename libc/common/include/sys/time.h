2070d4aa302b0d9d82ba8fd7077ac1a9b3994fb2
Alex Boxall <u7468248@anu.edu.au>
2024-05-27 21:33:26 +1000

    gettimeofday/settimeofday

--- libc/common/include/sys/time.h
+++ libc/common/include/sys/time.h
@@ -11,3 +11,6 @@ struct timezone {
     int tz_minuteswest;     /* minutes west of Greenwich */
     int tz_dsttime;         /* type of DST correction */
 };
+
+int gettimeofday(struct timeval* tv, struct timezone* restrict tz);
+int settimeofday(const struct timeval* tv, const struct timezone* tz);
---
+++ libc/hosted/systime/time.c
@@ -0,0 +1,35 @@
+#include <sys/time.h>
+#include <os/time.h>
+#include <errno.h>
+#include <timeconv.h>
+
+int gettimeofday(struct timeval* tv, struct timezone* restrict tz) {
+    (void) tz;
+
+    if (tv != NULL) {
+        uint64_t t = OsGetLocalTime();
+        if (t == 0) {
+            errno = EIO;
+            return -1;
+        }
+        tv->tv_sec = TimeValueToUnixTime(t);
+        tv->tv_usec = t % 1000000;
+    }
+
+    return 0;
+}
+
+int settimeofday(const struct timeval* tv, const struct timezone* tz) {
+    (void) tz;
+
+    uint64_t t = UnixTimeToTimeValue(tv->tv_sec);
+    t += tv->tv_usec;
+
+    int res = OsSetLocalTime(t);
+    if (res != 0) {
+        errno = res;
+        return -1;
+    }
+
+    return 0;
+}
