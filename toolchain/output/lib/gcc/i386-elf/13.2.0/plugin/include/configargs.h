efcf6de0b23a6faf22eee61627f7f1e2438ed68c
Alex Boxall <u7468248@anu.edu.au>
2024-06-01 12:49:57 +1000

    Removed toolchain due to license issues

--- toolchain/output/lib/gcc/i386-elf/13.2.0/plugin/include/configargs.h
+++
@@ -1,7 +0,0 @@
-/* Generated automatically. */
-static const char configuration_arguments[] = "../../source/gcc-13.2.0/configure --target=i386-elf --prefix=/Users/alex/Desktop/NOS/toolchain/output --disable-nls --enable-languages=c,c++,objc --disable-werror --without-headers --with-gmp=/opt/homebrew/Cellar/gmp/6.3.0 --with-mpfr=/opt/homebrew/Cellar/mpfr/4.2.1 --with-mpc=/opt/homebrew/Cellar/libmpc/1.3.1";
-static const char thread_model[] = "single";
-
-static const struct {
-  const char *name, *value;
-} configure_default_options[] = { { "cpu", "i386" }, { "arch", "i386" } };
